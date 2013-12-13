c0129dcd28555ceb9314d1f82024ce136ee0ea96
Andrew Kourosh <mail@spark.ms>
2013-12-13 17:33:48 +0400

    Login controller & view

--- inquiry.js
+++ inquiry.js
@@ -1,45 +1,55 @@
-var MainCtrl = function($scope, $http, endpoints) {
-
-    var token = false;
+var LoginCtrl = function($scope, $http, endpoints) {
 
     $scope.login = 'tester@test.com';
     $scope.pass = '';
-    $scope.loginError = false;
+    $scope.hasError = false;
+    $scope.working = false;
 
     // checks if the user is logged in
     $scope.showLogin = function() {
-        return !token || $scope.loginError;
+        return !$scope.token || $scope.hasError;
+    };
+
+    // checks if the form has been filled in
+    $scope.canLogIn = function() {
+        return !!$scope.login && !!$scope.pass && !$scope.working;
+    }
+
+    // closes error message
+    $scope.closeError = function() {
+        $scope.hasError = false;
+    };
+
+    // gets the caption for login button
+    $scope.loginCaption = function() {
+        return $scope.working ? 'Working...' : 'Log in';
     };
 
     // attempts login
     $scope.doLogin = function() {
-
+        $scope.working = true;
         var request = {
+            authenticationData: {
+                PlainTextPassword: $scope.pass,
+                SetPersistentCookie: true
+            },
             claimedIdentity: {
-                Email: $scope.login,
-                authenticationData:{
-                    PlainTextPassword: $scope.pass,
-                    SetPersistentCookie: true
-                }
+                Email: $scope.login
             }
         };
-
         $http.post(endpoints.sso + '/CombinedAuthenticate', request)
             .success(function(data, status) {
-                $scope.loginError = data.d.Success;
+                $scope.working = false;
+                $scope.hasError = !data.d.Success;
                 if(data.d.Success)
-                    token = data.d.Token;
-            })
-            .error(function(data, status) {
-                $scope.loginError = true;
+                    $scope.token = data.d.Token;
             });
     };
 };
 
-angular.module('inquiry')
-    .var('endpoints',
-        {
-            publ : 'http://publ.com/Services/Admin.asmx',
-            sso : 'https://logon.flippingbook.com/SSOThinClient.asmx'
-        })
-    .controller('MainCtrl', [ '$scope', '$http', 'endpoints', MainCtrl ]);
+angular.module('inquiry', [])
+    .value('endpoints', {
+        publ: 'http://publ.com/Services/Admin.asmx',
+        sso : 'https://logon.flippingbook.com/SSOThinClient.asmx'
+    })
+    .controller('LoginCtrl', [ '$scope', '$http', 'endpoints', LoginCtrl ]);
